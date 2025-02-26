12463ce3e3d3c790819ed90d51a5da9b3d8ed2f7
Hieu Nguyen <143702081+HiuuNguynn@users.noreply.github.com>
2025-02-26 14:50:33 +0000

    fix

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        if (env('APP_ENV') == 'production') {
+            $middleware->trustProxies(at: '*');
+        }
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
